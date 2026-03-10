decb51ac5b13f8cc9058714601b296be88cee444
57TerabyteZipBomb <gerrard.arkinara@gmail.com>
2026-03-10 17:12:53 +0700

    dash vfx fixes

--- Assets/Scripts/DashEffect.cs
+++ Assets/Scripts/DashEffect.cs
@@ -6,16 +6,20 @@ public class DashEffect : MonoBehaviour
 
     public void OnDash(Vector2 direction)
     {
+        float angle = 0f;
         // Rotate vfxContainer to face dash direction
         if (direction != Vector2.zero)
         {
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, 0f, angle);
         }
 
         // Play each particle system
         foreach (ParticleSystem particle in _particles)
         {
+            var main = particle.main;
+            main.startRotation = -angle * Mathf.Deg2Rad; // MainModule uses radians
+
             particle.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
             particle.Play();
         }
