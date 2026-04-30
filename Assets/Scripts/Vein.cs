519a6de47ef50646eac9c84a95b6ad7b7cdda2b2
GalihPPratama <galihpriadiwangsa@gmai.com>
2026-04-30 12:22:35 +0700

    Retiled

--- Assets/Scripts/Vein.cs
+++ Assets/Scripts/Vein.cs
@@ -24,6 +24,14 @@ public class Vein : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(boomerangTag))
+        {
+            Hit();
+        }
+    }
+
     private void Hit()
     {
         currentHealth -= 1;
